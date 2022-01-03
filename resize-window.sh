69f1406ec53187db904d8de0327d18b5ba5ea83b
Giles Mullins <Giles Mullins>
2022-01-03 16:08:27 +0000

    added variables for screensize and border

--- resize-window.sh
+++ resize-window.sh
@@ -1,16 +1,47 @@
 #!/usr/bin/sh
 
+# Declare variables
+
+SCREEN1WIDTH=5120
+SCREEN1HEIGHT=1440
+SCREEN2WIDTH=1920  # Not used
+SCREEN2HEIGHT=1080
+MASTERPANELPERC=6
+WIDTHDIFF=0
+HEIGHTDIFF=69
+BORDER=6
+G=0
+
+# Calculate Master and Slave widths
+SLAVEPANELPERC=$((10-MASTERPANELPERC))
+BORDEROFFSET=$((BORDER*2))
+MASTERWIDTH=$(($((SCREEN1WIDTH*MASTERPANELPERC/10))-BORDEROFFSET-BORDER))
+SLAVEWIDTH=$(($((SCREEN1WIDTH*SLAVEPANELPERC/10/2))-BORDEROFFSET))
+
+# Calculate X,Y,W,H
+XLEFT=$((0+BORDER))
+XMID=$((XLEFT+SLAVEWIDTH+BORDEROFFSET))
+XRIGHT=$((SCREEN1WIDTH-BORDEROFFSET-SLAVEWIDTH))
+Y=$((SCREEN2HEIGHT+BORDER))
+H=$((SCREEN1HEIGHT-HEIGHTDIFF-BORDEROFFSET))
+
+# Print geometry
+echo Left x:$XLEFT y:$Y w:$SLAVEWIDTH h:$H
+echo Mid x:$XMID y:$Y w:$MASTERWIDTH h:$H
+echo Right x:$XRIGHT y:$Y w:$SLAVEWIDTH h:$H
+
+# Take command line argument to determine placement on screen
 case $1 in
     mid)
-        wmctrl -r:ACTIVE: -e 0,1024,1080,3072,1371
+        wmctrl -r:ACTIVE: -e $G,$XMID,$Y,$MASTERWIDTH,$H
         ;;
     left)
-        wmctrl -r:ACTIVE: -e 0,4,1080,1024,1371
+        wmctrl -r:ACTIVE: -e $G,$XLEFT,$Y,$SLAVEWIDTH,$H
         ;;
     right)
-        wmctrl -r:ACTIVE: -e 0,4096,1080,1024,1371
+        wmctrl -r:ACTIVE: -e $G,$XRIGHT,$Y,$SLAVEWIDTH,$H
         ;;
     *)
         echo error
-        ;;
+       ;;
 esac
